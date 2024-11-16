35e3840834db3c6925388e9c534907f80e188e21
Luisa <grumnnie@gmail.com>
2024-11-16 19:22:00 -0300

    deploy parte 3

--- backend/src/db_config.js
+++ backend/src/db_config.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     host     : process.env.MYSQL_ADDON_HOST,
     database : process.env.MYSQL_ADDON_DB,
     user     : process.env.MYSQL_ADDON_USER,
-    password : process.env.MYSQL_ADDON_PASSWORD
+    password : process.env.MYSQL_ADDON_PASSWORD, 
+    port: process.env.MYSQL_ADDON_PORT
 });
 
 connection.connect((err) => {
